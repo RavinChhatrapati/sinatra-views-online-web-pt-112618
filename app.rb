3cdff0d6f0d72cc1b56ac8e8863018c665e71492
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-15 19:52:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 
-get '/' do           #embedded ruby
-	erb :index
+get '/' do           #embedded ruby lines 6 - 8 are routes.
+	erb :index          #routes tell how they interact with server. rendering an erb file
 end
 
 get "/info" do
@@ -12,7 +12,7 @@ get "/info" do
 end
 
 get "/info" do
-"Testing the info page"
+"Testing the info page"  # rendering = popping info on page
 end
 
 end
